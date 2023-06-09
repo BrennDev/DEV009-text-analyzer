49c4b2f858a57fc56c236cd5b519dbeada3a62f8
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-06-08 23:59:40 -0600

    Funcion recuento de caracteres sin espacios, funcion longitud media de palabras, Boton de borrar, Cambio test e2e

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,7 +1,7 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-      return text.trim().split(/\s+/).length;
+    return text.trim().split(/\s+/).length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
@@ -9,17 +9,23 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    var str= text.replace(/\s+/g, '')
+    const str= text.trim().replace(/[\s,.#!$%\^&\*;:{}=\-_`~()”“"…]+/g, '')
     return str.length;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    return ((text.trim().replace(/\s+/g, '').length)/(text.trim().split(/\s+/).length)).toFixed(2)
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const num= text.replace(/[^0-9]+/g, "");
+    return num
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let numSep= text.replace(/[^0-9]+/g, "").split("");
+    
+    return numSep
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-const textArea= document.getElementsByName('contenido')[0];
+const textArea= document.getElementsByName('user-input')[0];
 textArea.addEventListener('input',() => {
-    const value = textArea.value;
-    document.getElementsByClassName('wordCounter')[0].innerHTML='Palabras: '+analyzer.getWordCount(value) 
-    document.getElementsByClassName('characterCounter')[0].innerHTML='Caracteres: '+analyzer.getCharacterCount(value)
-    document.getElementsByClassName('characterCounterExcluding')[0].innerHTML='Caracteres sin espacios: '+analyzer.getCharacterCountExcludingSpaces(value)  
+  const value = textArea.value;
+  document.getElementsByClassName('wordCounter')[0].innerHTML='Palabras: '+analyzer.getWordCount(value); 
+  document.getElementsByClassName('characterCounter')[0].innerHTML='Caracteres: '+analyzer.getCharacterCount(value);
+  document.getElementsByClassName('characterCounterExcluding')[0].innerHTML='Caracteres sin espacios: '+analyzer.getCharacterCountExcludingSpaces(value);
+  document.getElementsByClassName('averageWordLength')[0].innerHTML='Promedio de longitud: '+analyzer.getAverageWordLength(value);
+  document.getElementsByClassName('numberCounter')[0].innerHTML='Números: '+analyzer.getNumberCount(value);
+  document.getElementsByClassName('numberSum')[0].innerHTML='Suma de números: '+analyzer.getNumberSum(value);
 });
 
+const resetButton=document.getElementById('reset-button');
+resetButton.addEventListener('click',resetAll);
+
+function resetAll(){
+  document.getElementsByName('user-input')[0].value=" ";
+  document.getElementsByClassName('wordCounter')[0].innerHTML='Palabras: 0'; 
+  document.getElementsByClassName('characterCounter')[0].innerHTML='Caracteres: 0';
+  document.getElementsByClassName('characterCounterExcluding')[0].innerHTML='Caracteres sin espacios: 0';
+  document.getElementsByClassName('averageWordLength')[0].innerHTML='Promedio de longitud: 0';
+  document.getElementsByClassName('numberCounter')[0].innerHTML='Números: 0';
+  document.getElementsByClassName('numberSum')[0].innerHTML='Suma de números: 0';
+}
