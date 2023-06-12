70fd0f6dc0425acb37deb88c5fa6281355f1f5b0
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-06-11 22:26:56 -0600

    Funcion de numeros y suma de numeros. Estilo con CSS

--- src/analyzer.js
+++ src/analyzer.js
@@ -9,7 +9,7 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    const str= text.trim().replace(/[\s,.#!$%\^&\*;:{}=\-_`~()”“"…]+/g, '')
+    const str= text.trim().replace(/[\s,.#!$%&;:{}=\-_`~()”“"…]+/g, '')
     return str.length;
   },
   getAverageWordLength: (text) => {    
@@ -18,15 +18,27 @@ const analyzer = {
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    const num= text.replace(/[^0-9]+/g, "");
-    return num
+    const words= text.split(" ");
+
+    const numberCount= [];
+    for (let i= 0; i < words.length; i++) {
+      if (!isNaN(parseInt(words[i]))) {
+        numberCount.push(words[i]);
+      }
+    } 
+    return numberCount;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    let numSep= text.replace(/[^0-9]+/g, "").split("");
-    
-    return numSep
+    const word= text.split(" ");
+
+    let numSum= 0;
+    for (let i= 0; i < word.length; i++) {
+      if (!isNaN(parseInt(word[i]))) {
+        numSum += parseInt(word[i]);
+      }
+    } 
+    return numSum;
   },
 };
-
 export default analyzer;
